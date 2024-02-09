51c84fa74cee3f3e999eb159ed1a893b1eadce5a
benumc <ben@paramountautomation.com>
2024-02-09 05:31:09 -0800

    Update app_logger.rb

--- app_logger.rb
+++ app_logger.rb
@@ -3,12 +3,11 @@
 require 'logger'
 
 class AppLogger
-  def self.setup(file_name)
+  def self.setup(file_name, log_path = Dir.home)
     script_name = File.basename(file_name, '.*')
-    log_path = "#{Dir.home('RPM')}/ruby/#{script_name}/"
+    log_path = "#{log_path.chomp('/')}/#{script_name}/"
     Dir.mkdir(log_path) unless Dir.exist?(log_path)
 
-    # Use $0 to dynamically name the log file based on the script name
     log_filename = "#{log_path}#{script_name}_#{Time.now.strftime('%Y-%m-%d')}.log"
 
     logger = Logger.new(log_filename, 10, 1024 * 1024 * 10)
