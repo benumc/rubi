0fd702185e9d5a9bd2788f0affc733269a3f39f5
benumc <ben@paramountautomation.com>
2024-02-10 16:09:12 -0800

    Update app_logger.rb

--- app_logger.rb
+++ app_logger.rb
@@ -6,7 +6,7 @@ class AppLogger
   def self.setup(file_name, log_path = Dir.home)
     script_name = File.basename(file_name, '.*')
     log_path = "#{log_path.chomp('/')}/#{script_name}/"
-    Dir.mkdir(log_path) unless Dir.exist?(log_path)
+    FileUtils.mkdir_p(log_path) unless Dir.exist?(log_path)
 
     log_filename = "#{log_path}#{script_name}_#{Time.now.strftime('%Y-%m-%d')}.log"
 
